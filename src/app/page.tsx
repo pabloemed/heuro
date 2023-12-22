344ad038ce52fdb693d21b19e9b36b743c61dfb6
Pablo Medina <pmedina@litebox.ai>
2023-12-22 14:16:36 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+import { useEffect } from 'react';
+import ReactPlayer from 'react-player';
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <video id='myVideo' width='500' height='500' controls>
+        <source
+          src='https://azurecloudfront-eqh0cueth4axgfgy.z01.azurefd.net/heuroassets/SampleVideo_1280x720_30mb.mp4'
+          type='video/mp4'
+        />
+        Your browser does not support the video tag.
+      </video>
+    </main>
+  );
+}
